10826b9d9e8bff4b68803601453bf72121ee238a
HK Yoon <hkyoon85@gmail.com>
2018-01-22 22:44:21 +0900

    Add Color component

---
+++ src/js/components/Color/index.js
@@ -0,0 +1,15 @@
+const colors = {
+  primary: '#3399ff',
+  black: '#4a4a4a',
+  grey: '#a4a4a4',
+  lightGrey: '#c0c0c0',
+  silver: '#d9e0e8',
+  lightSilver: '#ebf0f5',
+  white: '#ffffff',
+  green: '#46c8b4',
+  yellow: '#fed535',
+  red: '#ff6666',
+  purple: '#a082fa',
+};
+
+export default colors;
