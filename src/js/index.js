07f36a6b4220777ae51bbb051d0c66eef2a02af2
HK Yoon <hkyoon85@gmail.com>
2018-05-18 09:37:31 +0900

    Export Menu components

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,12 @@
 import Button from './components/Button';
 import Dropdown from './components/Dropdown';
 import List from './components/List';
+import { MenuSection, MenuItem } from './components/Menu';
 
 export {
   Button,
   Dropdown,
   List,
+  MenuSection,
+  MenuItem,
 };
