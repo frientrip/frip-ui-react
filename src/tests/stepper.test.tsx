dd9f7155214f42998517c0c84288deec957d0150
박기완 <zprime0920@gmail.com>
2018-08-07 16:19:21 +0900

    주어진 단계의 제목이 모두 표시되는지 테스트하는 코드 추가

--- src/tests/stepper.test.tsx
+++ src/tests/stepper.test.tsx
@@ -7,3 +7,11 @@ it('주어진 step만큼의 list가 생성되어야 한다.', () => {
   const wrapper = shallow(<Stepper currentStep={1} totalSteps={mockSteps}/>);
   expect(wrapper.find('li').length).toEqual(mockSteps.length); // 4
 });
+
+it('주어진 제목이 모두 표시된다.', () => {
+  const mockSteps = ['기본 정보 입력', '세부 사항 및 일정', '프립 소개 및 홍보', '추가 옵션'];
+  const wrapper = shallow(<Stepper currentStep={1} totalSteps={mockSteps} />);
+  expect(wrapper.find('li').map((node) => {
+    return node.text();
+  })).toEqual(mockSteps); // 4
+});
