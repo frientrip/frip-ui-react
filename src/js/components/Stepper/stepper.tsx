9edc4b940a5371502b5fd26d4c4a2cc4948887f6
박기완 <zprime0920@gmail.com>
2018-08-08 16:33:00 +0900

    Refactor: interface 위치 변경

--- src/js/components/Stepper/stepper.tsx
+++ src/js/components/Stepper/stepper.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
 
 /**
- * Stepper에서 사용하는 props interface
+ * Stepper UI Compoment
+ * currentStep은 0부터 시작
  */
 interface Props {
   currentStep: number;
   totalSteps: string[];
 }
-
-/**
- * Stepper에서 사용하는 state interface
- */
 interface State {
   currentStep: number;
   totalSteps: string[];
 }
-
-/**
- * Stepper UI Compoment
- * currentStep은 0부터 시작
- */
 export default class Stepper extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
