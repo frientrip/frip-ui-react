3d699c53bd2a85bde033cb62ab81d260a3a3f6fd
박기완 <zprime0920@gmail.com>
2018-08-09 10:35:14 +0900

    Fix: ES6, svg 때문에 Jest가 제대로 작동하지 않는 문제 수정

--- jest.config.js
+++ jest.config.js
@@ -72,10 +72,13 @@ module.exports = {
     'tsx',
     'js',
     'json',
+    '.svg',
   ],
 
   // A map from regular expressions to module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -166,7 +169,8 @@ module.exports = {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest"
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
