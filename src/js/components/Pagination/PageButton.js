c860bbc41452a0bf5bb577a80196f827c43c7932
박기완 <zprime0920@gmail.com>
2018-09-18 16:32:49 +0900

    Fix: pagination disabled 상태일 때 디자인 수정

--- src/js/components/Pagination/PageButton.js
+++ src/js/components/Pagination/PageButton.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Color from '../Color';
-import leftArrow from '../../assets/svgs/small-prev-black.svg';
-import rightArrow from '../../assets/svgs/small-next-black.svg';
+import leftArrow from '../../assets/svgs/small-prev.svg';
+import rightArrow from '../../assets/svgs/small-next.svg';
 
 const propTypes = {
   label: PropTypes.string,
@@ -35,41 +35,34 @@ const Button = styled.div`
   color: ${Color.black};
   background-color: white;
   text-align: center;
-  cursor: pointer;
+  cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
   margin: 4px;
   user-select: none;
   font-weight: ${({ isSelected }) => (isSelected ? 'bold' : 'normal')};
 
   &:hover,
   &:focus {
-    background-color: ${Color.veryLightBlue};
+    background-color: ${({ disabled }) => (disabled ? 'white' : Color.veryLightBlue)};
   }
-`;
-
-const Blocker = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  cursor: not-allowed;
-  background-color: white;
-  opacity: 0.7;
-  z-index: 100;
 `;
 
 const IconWrapper = styled.div`
   display: inline-block;
   width: 24px;
   height: 24px;
+
+  color: ${({ disabled }) => (disabled ? '#e6e6e6' : '#4a4a4a')};
+
+  svg {
+    stroke: currentColor;
+  }
 `;
 
 const PageButton = ({ label, isSelected, disabled, onClick, icon }) => (
   <Wrapper>
-    {disabled && <Blocker />}
-    <Button onClick={onClick} isSelected={isSelected}>
+    <Button onClick={onClick} isSelected={isSelected} disabled={disabled}>
       {icon
-        ? <IconWrapper dangerouslySetInnerHTML={{ __html: icon }} />
+        ? <IconWrapper disabled={disabled} dangerouslySetInnerHTML={{ __html: icon }} />
         : label}
     </Button>
   </Wrapper>
