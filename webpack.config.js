f95f301ba383203279b7bf8b53142c433ecfa20a
박기완 <zprime0920@gmail.com>
2018-11-06 13:52:36 +0900

    minified js 파일을 생성하도록 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,49 @@
-const path = require("path");
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: ["./src/js/index.ts"],
+  entry: ['./src/js/index.ts'],
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "js/[name].js",
-    libraryTarget: "commonjs2"
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js',
+    libraryTarget: 'commonjs2',
   },
   externals: {
     'styled-components': {
       commonjs: 'styled-components',
       commonjs2: 'styled-components',
-      amd: 'styled-components'
-    }
+      amd: 'styled-components',
+    },
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
-        loader: require.resolve("ts-loader"),
+        loader: require.resolve('ts-loader'),
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.svg$/,
-        use: ["svg-inline-loader"]
-      }
-    ]
+        use: ['svg-inline-loader'],
+      },
+    ],
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', 'jsx'],
   },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+    }),
+  ],
 };
