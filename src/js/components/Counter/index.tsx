c1b831a0b5d8717700a721e4efbf7b88c858f4a6
박기완 <zprime0920@gmail.com>
2018-11-13 10:27:35 +0900

    Feature: 값 부분 input으로 변경

--- src/js/components/Counter/index.tsx
+++ src/js/components/Counter/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import '../../../css/reset.css';
 import MinusIcon from '../../assets/svgs/minus-black.svg';
 import PlusIcon from '../../assets/svgs/plus-black.svg';
 import colors from '../Color';
@@ -14,14 +15,14 @@ const Wrapper = styled.div`
   align-items: center;
   border: solid 1px #e6e6e6;
   border-radius: 4px;
-  max-width: 180px;
+  width: 180px;
   height: 40px;
 `;
 
-const MinusButton = styled.div`
+const ControlButton = styled.div`
+  flex: 0 0 40px;
   width: 40px;
   height: 40px;
-  border-right: solid 1px #e6e6e6;
   padding: 12px;
   line-height: 16px;
 
@@ -33,34 +34,22 @@ const MinusButton = styled.div`
   }
 `;
 
-const Value = styled.div`
+const Value = styled.input`
   width: 100px;
-  padding: 9px 16px;
-  text-align: center;
-  user-select: none;
-`;
-
-const PlusButton = styled.div`
-  width: 40px;
-  height: 40px;
+  border: 0;
   border-left: solid 1px #e6e6e6;
-  padding: 12px;
-  line-height: 16px;
-
-  &:hover {
-    background-color: ${colors.veryLightBlue};
-  }
-  &:active {
-    background-color: ${colors.babyBlue}
-  }
+  border-right: solid 1px #e6e6e6;
+  padding: 9px 16px;
+  font-size: 14px;
+  text-align: center;
 `;
 
 const Counter: React.SFC<CounterProps> = (props) => {
   return (
     <Wrapper>
-      <MinusButton dangerouslySetInnerHTML={{ __html: MinusIcon }} onClick={() => props.onChange(props.value - 1)} />
-      <Value>{props.value}</Value>
-      <PlusButton dangerouslySetInnerHTML={{ __html: PlusIcon }} onClick={() => props.onChange(props.value + 1)} />
+      <ControlButton dangerouslySetInnerHTML={{ __html: MinusIcon }} onClick={() => props.onChange(props.value - 1)} />
+      <Value type="number" value={props.value} onChange={event => props.onChange(Number(event.target.value))} onWheel={e => e.preventDefault()} />
+      <ControlButton dangerouslySetInnerHTML={{ __html: PlusIcon }} onClick={() => props.onChange(props.value + 1)} />
     </Wrapper>
   );
 };
